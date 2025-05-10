5f696b9107940549aaf0992bbc7832bc916e8f61
sakethp7 <163278054+sakethp7@users.noreply.github.com>
2025-05-10 14:09:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -420,6 +420,11 @@
 # if __name__ == "__main__":
 #     main()
 
+
+__import__('pysqlite3')
+import sys
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import requests
 import os
